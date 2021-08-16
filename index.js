bb19c51d6c00ccc22d0fbd0746713a58d495fc6b
BRANDON L <brandonlaubaugh@gmail.com>
2021-08-16 18:52:05 -0400

    sixth commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 require('dotenv').config()
 
+const config = require('./config/app')
+
 const app = express()
 
 
@@ -15,7 +17,7 @@ app.get('/login', (req,res) => {
     return res.send('Login Screen')
 })
 
-const port = process.env.APP_PORT
+const port = config.appPort
 
 app.listen(port, () => {
     console.log(`Server Listening on port ${port}`);
